51a218ac18f249c67a53708033945cfd13bb70dd
jorge <jorgecsri@gmail.com>
2022-10-06 18:26:30 -0400

    prueba1

---
+++ app/Http/Controllers/AdministradorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\administrador;
+use Illuminate\Http\Request;
+
+class AdministradorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\administrador  $administrador
+     * @return \Illuminate\Http\Response
+     */
+    public function show(administrador $administrador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\administrador  $administrador
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(administrador $administrador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\administrador  $administrador
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, administrador $administrador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\administrador  $administrador
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(administrador $administrador)
+    {
+        //
+    }
+}
---
+++ app/Models/administrador.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class administrador extends Model
+{
+    use HasFactory;
+}
