539579bd96650e849dce590dd9ad4facf786d422
jorge <jorgecsri@gmail.com>
2022-10-06 18:35:10 -0400

    backend2

---
+++ app/Http/Controllers/TabletaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tableta;
+use Illuminate\Http\Request;
+
+class TabletaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tableta  $tableta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tableta $tableta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tableta  $tableta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tableta $tableta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tableta  $tableta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tableta $tableta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tableta  $tableta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tableta $tableta)
+    {
+        //
+    }
+}
