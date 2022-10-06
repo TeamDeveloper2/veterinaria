ad61afdb52183aef4a6a97c3c4f76bcd3df83def
MurilloDiana <dianamurillo.467@gmail.com>
2022-10-05 22:02:43 -0400

    plantilla mas login

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>MKI: @yield('titulo')</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+
+    <!-- Toastr style -->
+    <link href="{{asset('css/plugins/toastr/toastr.min.css')}}" rel="stylesheet">
+
+    <!-- Gritter -->
+    <link href="{{asset('js/plugins/gritter/jquery.gritter.css')}}" rel="stylesheet">
+
+    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+    <link href="{{asset('css/plugins/iCheck/custom.css')}}" rel="stylesheet">
+    <link href="{{asset('css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css')}}" rel="stylesheet">
+
+    <link href="{{asset('css/plugins/dataTables/datatables.min.css')}}" rel="stylesheet">
+
+    <!-- Mainly scripts -->
+    <script src="{{asset('js/jquery-3.1.1.min.js')}}"></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+    <script src="{{asset('js/plugins/metisMenu/jquery.metisMenu.js')}}"></script>
+    <script src="{{asset('js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+
+    <!-- Flot -->
+    <script src="{{asset('js/plugins/flot/jquery.flot.js')}}"></script>
+    <script src="{{asset('js/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>
+    <script src="{{asset('js/plugins/flot/jquery.flot.spline.js')}}"></script>
+    <script src="{{asset('js/plugins/flot/jquery.flot.resize.js')}}"></script>
+    <script src="{{asset('js/plugins/flot/jquery.flot.pie.js')}}"></script>
+
+    <!-- Peity -->
+    <script src="{{asset('js/plugins/peity/jquery.peity.min.js')}}"></script>
+    <script src="{{asset('js/demo/peity-demo.js')}}"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{{asset('js/inspinia.js')}}"></script>
+    <script src="{{asset('js/plugins/pace/pace.min.js')}}"></script>
+
+    <!-- jQuery UI -->
+    <script src="{{asset('js/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+
+    <!-- GITTER -->
+    <script src="{{asset('js/plugins/gritter/jquery.gritter.min.js')}}"></script>
+
+    <!-- Sparkline -->
+    <script src="{{asset('js/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+
+    <!-- Sparkline demo data  -->
+    <script src="{{asset('js/demo/sparkline-demo.js')}}"></script>
+
+    <!-- ChartJS-->
+    <script src="{{asset('js/plugins/chartJs/Chart.min.js')}}"></script>
+
+    <!-- Toastr -->
+    <script src="{{asset('js/plugins/toastr/toastr.min.js')}}"></script>
+
+    <!-- iCheck -->
+    <script src="{{asset('js/plugins/iCheck/icheck.min.js')}}"></script>
+    <!-- Typehead -->
+    <script src="{{asset('js/plugins/typehead/bootstrap3-typeahead.min.js')}}"></script>
+
+    <script src="{{asset('js/plugins/dataTables/datatables.min.js')}}"></script>
+    <script src="{{asset('js/plugins/dataTables/dataTables.bootstrap4.min.js')}}"></script>
+    
+    <script src="{{asset('js/jquery.PrintArea.js')}}"></script>
+
+        <script>
+            $(document).ready(function () {
+                $('.i-checks').iCheck({
+                    checkboxClass: 'icheckbox_square-green',
+                    radioClass: 'iradio_square-green',
+                });
+            });
+        </script>
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar-default navbar-static-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav metismenu" id="side-menu">
+                    <li class="nav-header">
+                        <div class="dropdown profile-element">
+                            <img alt="image" class="rounded-circle" width="60px" src="{{asset('img/noUser.jpg')}}"/>
+                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                                <span class="block m-t-xs font-bold">{{session('nombre')}} <b class="caret"></b></span>
+                            </a>
+                            <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                                {{-- <li><a class="dropdown-item" href="profile.html">Cambiar Contrase&ntilde;a</a></li>
+                                <li class="dropdown-divider"></li> --}}
+                                <li><a class="dropdown-item" href="{{route('cerrar.session')}}">Logout</a></li>
+                            </ul>
+                        </div>
+                        <div class="logo-element">
+                            MKI
+                        </div>
+                    </li>
+                    <li <?=$parControl['modulo']=='home'?'class="active"':'' ?> >
+                        <a href="{{route('index')}}"><i class="fa fa-home"></i> <span class="nav-label">Home </span></a>
+                    </li>
+                    @foreach(session('permisos') as $modulo)
+                        <li data-id="{{$modulo['id']}}" <?=$parControl['modulo']==$modulo['codigo']?'class="active"':'' ?>>
+                            <a href="javascript:void(0)"><i class="fa {{$modulo['icono']}}"></i> <span class="nav-label">{{$modulo['titulo']}}</span> <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                @foreach($modulo['funcionalidades'] as $funcionalidad)
+                                    <li <?=$parControl['funcionalidad']==$funcionalidad['ruta']?'class="active"':'' ?>><a href="{{route($funcionalidad['ruta'].'.'.$funcionalidad['accion'])}}">{{$funcionalidad['titulo']}}</a></li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endforeach
+                </ul>
+                    
+            </div>
+        </nav>
+    
+        <div id="page-wrapper" class="gray-bg dashbard-1">
+        <div class="row border-bottom">
+        <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+            <form role="search" class="navbar-form-custom" action="search_results.html">
+                
+            </form>
+        </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+                    <a href="{{route('cerrar.session')}}">
+                        <i class="fa fa-sign-out"></i> Cerrar Sesion
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        </div>
+
+
+        @yield('content')
+
+        
+        <div class="footer">
+            <div class="float-right">
+                UAGRM - FICCT
+            </div>
+            <div>
+                <strong>Copyright</strong> GRUPO G  &copy; 2021
+            </div>
+        </div>
+        
+    </div>
+</div>
+</body>
+</html>
