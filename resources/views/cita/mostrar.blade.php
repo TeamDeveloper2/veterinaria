72c22a5202d926c7077449f6c67389583f20702c
MurilloDiana <dianamurillo.467@gmail.com>
2022-10-09 22:34:41 -0400

    cliente y cita
    elizabeth

---
+++ resources/views/cita/mostrar.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+
+@section('content')
+    <div class="row wrapper border-bottom white-bg page-heading">
+        <div class="col-lg-12">
+            <h2>Mostrar Cita</h2>
+        </div>
+    </div>
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox ">
+
+                    <div class="ibox-content">
+                        <form >
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Nombre del propietario</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Nombre de tu mascota</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Teléfono</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Genero</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Email1</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Mensaje</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+
+
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Enviar</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@stop
---
+++ resources/views/cliente/agregar.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row wrapper border-bottom white-bg page-heading">
+        <div class="col-lg-12">
+            <h2>Agregar Cliente</h2>
+        </div>
+    </div>
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox ">
+
+                    <div class="ibox-content">
+                        <form action="" method="post">
+
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Nombres:<i class="text-danger">*</i></label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="nombres" value="" required="">
+                                </div>
+                            </div>
+                            @error('nombres')
+                                <div class="alert alert-danger alert-dismissable">"inserte mensaje aca si es necesario"<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button></div>
+                            @enderror
+                            {{-- comienzo --}}
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Teléfono:<i class="text-danger">*</i></label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="telefono" value="" required="">
+                                </div>
+                            </div>
+                            @error('telefono')
+                                <div class="alert alert-danger alert-dismissable">"inserte mensaje aca si es necesario"<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button></div>
+                            @enderror
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Dirección:<i class="text-danger">*</i></label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="direccion" value="" required="">
+                                </div>
+                            </div>
+                            @error('direccion')
+                                <div class="alert alert-danger alert-dismissable">"inserte mensaje aca si es necesario"<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button></div>
+                            @enderror
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Correo:<i class="text-danger">*</i></label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="correo" value="" required="">
+                                </div>
+                            </div>
+                            @error('correo')
+                                <div class="alert alert-danger alert-dismissable">"inserte mensaje aca si es necesario"<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button></div>
+                            @enderror
+
+                            <div class="form-group row">
+                                <div class="col-sm-4 col-sm-offset-2">
+                                    <button class="btn btn-success " type="submit">Registrar</button>
+                                    <button class="btn btn-danger " type="button" onclick="">Cancelar</button>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@stop
