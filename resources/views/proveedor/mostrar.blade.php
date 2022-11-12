fb8a04812dbee36370f44f186ec9e958aac0185f
MurilloDiana <dianamurillo.467@gmail.com>
2022-11-11 23:38:29 -0400

    proveedores

---
+++ resources/views/proveedor/mostrar.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.master')
+
+
+@section('content')
+    <div class="row wrapper border-bottom white-bg page-heading">
+        <div class="col-lg-12">
+            <h2>Informacion del Proveedor</h2>
+        </div>
+    </div>
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox ">
+
+                    <div class="ibox-content">
+                        <form >
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Nombre</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Apellido Paterno</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Apellido Materno</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">CI</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Nombre de Empresa</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Direccion de Empresa</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Telefono de Referencia</label>
+                                <div class="col-sm-10"><input type="text" class="form-control" value="" disabled=""></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@stop
