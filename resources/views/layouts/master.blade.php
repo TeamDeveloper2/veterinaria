1f279578f2d078c7098146a2c1439c6db2aa1e83
Mendoza210727 <mendoza.mc27@gmail.com>
2022-12-14 21:32:09 -0400

    modificacion prueba

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>veterinaria</title>
+    <title>veterinaria El Cristo</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
